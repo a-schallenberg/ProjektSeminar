34400fa33850a1e8743267d493c92af1f9c7c25c
Griszder <griszder@gmail.com>
2021-10-15 00:29:22 +0200

    Tested JNetwork => learn process does not work correctly (look on FIXME in JNetwork.backpropagation)
    FIXME
    backpropagation of JNetwork
    
    TODO
    Test Networks

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ import main.objectoriented.JNetwork;
 public class Main {
 
 	public static void main(String[] args) {
-		testJMnist();
+		testJOR();
 	}
 
 	private static void testPyXOR() {
@@ -18,10 +18,10 @@ public class Main {
 	}
 
 	private static void testJXOR() {
-		JNetwork network = new JNetwork(2, 1, 8, 8);
+		JNetwork network = new JNetwork(2, 3, 1);
 		double[][] input = new double[][] {{1, 1}, {1, 0}, {0, 1}, {0, 0}};
 		double[][] labels = new double[][] {{0}, {1}, {1}, {0}};
-		network.train(input, labels, 50000, 0.01);
+		network.train(input, labels, 10, 0.01);
 	}
 
 	private static void testPyMnist() {
@@ -30,7 +30,21 @@ public class Main {
 	}
 
 	private static void testJMnist() {
-		JNetwork network = new JNetwork(Mnist.TEST_IMAGES[0].length, 10, 16, 16);
+		JNetwork network = new JNetwork(Mnist.TEST_IMAGES[0].length, 16, 16, 10);
 		network.train(Mnist.TRAIN_IMAGES, Mnist.TRAIN_LABELS, 1000, 0.01);
+	}
+
+	private static void testJOR() {
+		JNetwork network = new JNetwork(2, 1);
+		double[][] input = new double[][] {{1, 1}, {1, 0}, {0, 1}, {0, 0}};
+		double[][] labels = new double[][] {{1}, {1}, {1}, {0}};
+		network.train(input, labels, 100000, 0.1);
+	}
+
+	private static void testJAND() {
+		JNetwork network = new JNetwork(2, 1);
+		double[][] input = new double[][] {{1, 1}, {1, 0}, {0, 1}, {0, 0}};
+		double[][] labels = new double[][] {{1}, {0}, {0}, {0}};
+		network.train(input, labels, 10000, 0.01);
 	}
 }
--- src/main/objectoriented/JNetwork.java
+++ src/main/objectoriented/JNetwork.java
@@ -12,18 +12,30 @@ public class JNetwork implements INetwork {
 	private final JNeuron[][] layers;
 	private final int inputSize;
 
+	/**
+	 * Creates a neural network which works with {@link JNeuron}s.
+	 * @param weights weights.length <=> Number of layers (without input layer).
+	 *                weights[i].length <=> Number of Units in Layer i.
+	 *                weights[i][j] <=> Incoming weights of Unit j of Layer i.
+	 *                weights[0][0] <=> size of each input vector.
+	 *                Condition 1: weights[i][j].length == weights[i-1].length.
+	 *                Condition 2: weights[0][0] == weights[0][j], for every j in range.
+	 * @throws  IllegalArgumentException Whether one of the condition of argument weights is false.
+	 */
 	public JNetwork(double[][][] weights) {
 		this(new SigmoidFunction(), weights);
 	}
 
 	/**
-	 *
-	 * @param function activation or output function
+	 * Creates a neural network which works with {@link JNeuron}s.
+	 * @param function activation or output function which will be used in the network.
 	 * @param weights weights.length <=> Number of layers (without input layer).
 	 *                weights[i].length <=> Number of Units in Layer i.
-	 *                weights[i][j].length = weights[i-1].length.
 	 *                weights[i][j] <=> Incoming weights of Unit j of Layer i.
-	 *                weights[0][0] = weights[0][j] <=> size of each input vector.
+	 *                weights[0][0] <=> size of each input vector.
+	 *                Condition 1: weights[i][j].length == weights[i-1].length.
+	 *                Condition 2: weights[0][0] == weights[0][j], for every j in range.
+	 * @throws  IllegalArgumentException Whether one of the condition of argument weights is false.
 	 */
 	public JNetwork(AFunction function, double[][][] weights) {
 		this.function = function;
@@ -44,10 +56,27 @@ public class JNetwork implements INetwork {
 		}
 	}
 
+	/**
+	 * Creates a neural network which works with {@link JNeuron}s.
+	 * @param numLayerUnits numLayerUnits.length <=> Number of layers.
+	 *                      numLayerUnits[i] <=> Number of units in layer i.
+	 *                      First layer <=> input layer
+	 *                      Last layer <=> output layer
+	 * @throws IllegalArgumentException Whether the argument's length is less than two, because the network needs at least an input layer and an output layer.
+	 */
 	public JNetwork(int... numLayerUnits) {
 		this(new SigmoidFunction(), numLayerUnits);
 	}
 
+	/**
+	 * Creates a neural network which works with {@link JNeuron}s.
+	 * @param function activation or output function which will be used in the network.
+	 * @param numLayerUnits numLayerUnits.length <=> Number of layers.
+	 *                      numLayerUnits[i] <=> Number of units in layer i.
+	 *                      First layer <=> input layer
+	 *                      Last layer <=> output layer
+	 * @throws IllegalArgumentException Whether the argument's length is less than two, because the network needs at least an input layer and an output layer.
+	 */
 	private JNetwork(AFunction function, int... numLayerUnits) {
 		if(numLayerUnits.length < 2)
 			throw  new IllegalArgumentException("Network need at least an input layer and an output layer (at least two integer arguments required)");
@@ -77,18 +106,23 @@ public class JNetwork implements INetwork {
 	public void train(double[][] input, double[][] labels, int repetitions, double learnRate){
 		double[][] results = new double[layers.length][];
 
+		System.out.println("Start training");
+		long start = System.currentTimeMillis();
+
 		for(int i = 0; i < repetitions; i++) {
 			double cost = 0d;
 			int correct = 0;
-			System.out.println("Repetition " + i);
+			System.out.println("Repetition " + (i + 1));
 			for(int j = 0; j < input.length; j++) {
 				forwardPropagation(input[j], results);
 				cost += cost(results[results.length - 1], labels[j]);
 				correct += correct(results[results.length - 1], labels[j]) ? 1 : 0;
 				backpropagation(results, labels[j], learnRate);
 			}
-			System.out.printf("Cost: %f\nCorrect: %d of %d\n\n", cost, correct, input.length);
+			System.out.printf("Cost: %f\nCorrect: %f%% \n\n", cost, correct/ (double) input.length * 100);
 		}
+
+		System.out.println("Finished training in " + (System.currentTimeMillis() - start) + "ms");
 	}
 
 	private void forwardPropagation(double[] input, double[][] results) {
@@ -102,7 +136,7 @@ public class JNetwork implements INetwork {
 
 	private void backpropagation(double[][] results, double[] label, double learnRate) {
 		double[] delta = new double[Util.maxLength(results)];
-		for(int i = 0; i < delta.length; i++)
+		for(int i = 0; i < label.length; i++)
 			delta[i] = 2 * (results[results.length - 1][i] - label[i]);
 
 		backpropagation(delta, results.length - 1, layers, results, learnRate);
@@ -120,7 +154,7 @@ public class JNetwork implements INetwork {
 				layers[index][i].weights[j] += -learnRate * results[index][i] * df * deltaI;
 
 			layers[index][i].bias += -learnRate * df * deltaI;
-			delta[i] = w * df * deltaI;
+			delta[i] = w * df * deltaI;                                 // FIXME Lernprozess funktioniert nur ohne hidden layers. Delta?
 		}
 
 		backpropagation(delta, index - 1, layers, results, learnRate);
